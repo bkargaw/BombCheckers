667f4685c4ccb993a35f7f2ae6e23de4c1d1036e
Bruk Argaw <bkargaw@gmail.com>
2017-03-28 09:52:31 -0700

    finished initial setup of the board

--- bundle.js
+++ bundle.js
@@ -68,41 +68,67 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
+const Board = __webpack_require__(2);
 
 let ctx;
+let board;
+
 $( ()=>{
   let c = document.getElementById("canvas");
   c.width  = 600;
   c.height = 600;
   ctx = c.getContext("2d");
+  board = new Board();
   draw();
 });
 
 
-function draw() {
-  let colors = _createColorForGrid();
-  for (let i = 0 ; i < 8; i++){
-    for (let j = 0 ; j < 8; j++){
-      let x = i * 75 ;
-      let y = j * 75;
-      ctx.beginPath();
-      ctx.rect(x, y, 75, 75);
-      ctx.fillStyle = colors[i][j];
-      ctx.fill();
+  function draw() {
+    drawBackGround();
+    drawThePieces();
+  }
+
+  function drawBackGround() {
+    let colors = _createColorForGrid();
+    for (let i = 0 ; i < 8; i++){
+      for (let j = 0 ; j < 8; j++){
+        let x = i * 75 ;
+        let y = j * 75;
+        ctx.beginPath();
+        ctx.rect(x, y, 75, 75);
+        ctx.fillStyle = colors[i][j];
+        ctx.fill();
+      }
     }
   }
-  drawImges();
-}
 
-function drawImges(){
-  let baseImage = new Image();
-  baseImage.src = './asset/images/bomb-fire.png';
-  baseImage.onload = function(){
-  ctx.drawImage(baseImage, 0 , 0, 75, 75 );
-};
-}
+  function drawThePieces(){
+    let piece;
+    let scale = 75;
+    for (let i = 0; i < 8 ; i++){
+      for(let j = 0; j < 8 ; j++){
+        piece = board.pieces[i][j];
+        if( piece ){
+          let scaledX = piece.x;
+          let scaledY = piece.y;
+          let img = piece.getImage();
+          if(img.complete) { //check if image was already loaded by the browser
+            drawAPiece(img, scaledX , scaledY, scale);
+          }else {
+            img.onload = ()=> drawAPiece(img, scaledX , scaledY, scale);
+          }
+
+        }
+      }
+    }
+  }
+
+  function drawAPiece(baseImage, x , y, scale){
+    ctx.drawImage(baseImage, x * scale , y * scale, 75, 75 );
+  }
+
 
 function _createColorForGrid(color1 ='gray', color2 = 'red'){
   let colorGrid = new Array(8);
@@ -121,6 +147,421 @@ function _createColorForGrid(color1 ='gray', color2 = 'red'){
   }
   return colorGrid;
 }
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+class Piece {
+  constructor(side , board, pos){
+    this.side = side;
+    this.board = board;
+    this.x = pos[0];
+    this.y = pos[1];
+    this.Piecetype = 'pawn';
+    this.isKing = false;
+    this.haskilledthisturn = false;
+    this.kingImage = this.setKingImage();
+    this.pieceImage =  this.setPieceImage();
+  }
+
+  setPos(pos){
+    this.x = pos[0];
+    this.y = pos[1];
+  }
+
+
+
+  getSide() {
+		return this.side;
+	}
+
+  getboard() {
+    return this.board;
+	}
+
+  getPiecetype() {
+    return this.Piecetype;
+	}
+
+  getHaskilledthisturn() {
+		return this.haskilledthisturn;
+	}
+
+  setHaskilledthisturn(haskilledthisturn) {
+		this.haskilledthisturn = haskilledthisturn;
+  }
+
+  setKing(isKing) {
+		this.isKing = isKing;
+	}
+
+  setPiecetype(Piecetype) {
+		this.Piecetype = Piecetype;
+	}
+
+  side() {
+		return this.side;
+	}
+
+
+  getImage(){
+    if (this.isKing){
+      return this.kingImage;
+    }else {
+      return this.pieceImage;
+    }
+  }
+
+  setKingImage(){
+    let kingImage = new Image();
+    if(this.side === 'red'){
+      kingImage.src = './asset/images/pawn-fire-crowned.png';
+    }else{
+      kingImage.src =  './asset/images/pawn-water-crowned.png';
+    }
+    return kingImage;
+  }
+
+  setPieceImage(){
+    let PawnImg =  new Image();
+    if(this.side === 'red'){
+      PawnImg.src = './asset/images/pawn-fire.png';
+    }else{
+      PawnImg.src =  './asset/images/pawn-water.png';
+    }
+    return PawnImg;
+  }
+
+
+	blowUp(x, y) {
+    if( this.board.grid()[x][y] !== null) {
+      this.board.remove(x,y);
+    }
+	}
+
+  // only applies to bomb pieces
+  explode( x, y) {
+
+   }
+	/**
+	 * Signals that this Piece has begun to capture (as in it captured a Piece)
+	 */
+  startCapturing() {
+		this.haskilledthisturn=true;
+	}
+
+	/**
+	 * Returns whether or not this piece has captured this turn
+	 */
+  hasCaptured() {
+		return this.haskilledthisturn;
+	}
+
+	/**
+	 * Resets the Piece for future turns
+	 */
+
+  finishCapturing() {
+		this.haskilledthisturn = true;
+	}
+}
+
+module.exports = Piece;
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const Piece = __webpack_require__(1);
+const BombPiece = __webpack_require__(3);
+const ShieldPiece = __webpack_require__(4);
+
+class Board {
+  constructor() {
+    this.pieces = this.setBoardUp();
+  }
+
+  setBoardUp(){
+    let grid = new Array(8);
+    for (let i = 0 ; i< 8 ; i++){
+      grid[i] = new Array(8);
+      for(let j = 0 ; j < 8 ; j++){
+        grid[i][j]= null;
+      }
+    }
+    // red classic
+    let R1 = new Piece('red', this, [0, 0]);
+    let R2 = new Piece('red', this, [2, 0]);
+		let R3 = new Piece('red', this, [4, 0]);
+    let R4 = new Piece('red', this, [6, 0]);
+		// red Shields
+		let R5 = new ShieldPiece('red', this, [1, 1]);
+    let R6 = new ShieldPiece('red', this, [3, 1]);
+		let R7 = new ShieldPiece('red', this, [5, 1]);
+    let R8 = new ShieldPiece('red', this, [7, 1]);
+		// red bomb
+		let R9 = new BombPiece('red', this, [0, 2]);
+    let R10 = new BombPiece('red', this, [2, 2]);
+		let R11 = new BombPiece('red', this, [4, 2]);
+    let R12 = new BombPiece('red', this, [6, 2]);
+
+    // blue classic
+		let B1 = new Piece('blue', this, [7, 7]);
+    let B2 = new Piece('blue', this, [5, 7]);
+    let B3 = new Piece('blue', this, [3, 7]);
+    let B4 = new Piece('blue', this, [1, 7]);
+		// Blue Shields
+		let B5 = new ShieldPiece('blue', this, [0, 6]);
+    let B6 = new ShieldPiece('blue', this, [2, 6]);
+    let B7 = new ShieldPiece('blue', this, [4, 6]);
+    let B8 = new ShieldPiece('blue', this, [6, 6]);
+		// Blue bomb
+		let B9 = new BombPiece('blue', this, [7, 5]);
+    let B10 = new BombPiece('blue', this, [5, 5]);
+		let B11 = new BombPiece('blue', this, [3, 5]);
+    let B12 = new BombPiece('blue', this, [1, 5]);
+
+      // RED SIDE
+		grid[0][0] = R1; grid[2][0] = R2; grid[4][0] = R3; grid[6][0] = R4;
+		grid[1][1] = R5; grid[3][1] = R6; grid[5][1] = R7; grid[7][1] = R8;
+    grid[0][2] = R9; grid[2][2] = R10; grid[4][2] = R11; grid[6][2] = R12;
+
+      // BLUE SIDE
+		grid[7][7] = B1; grid[5][7] = B2; grid[3][7] = B3; grid[1][7] = B4;
+		grid[0][6] = B5; grid[2][6] = B6; grid[4][6] = B7; grid[6][6] = B8;
+		grid[7][5] = B9; grid[5][5] = B10; grid[3][5] = B11; grid[1][5] = B12;
+
+    return grid;
+  }
+
+
+
+  _isInbound(x, y){
+    if ( x > 0 && x <= 7  || y > 0 || y <= 7 ) return true;
+    return false;
+  }
+  /*
+   Gets the piece at position (x, y) on the board, or null if there is no piece.
+   If (x, y) are out of bounds, returns null.
+   */
+  pieceAt( x, y){
+    if (!(this._isInbound())) return null;
+    return this.pieces[x][y];
+  }
+
+  /* Places p at (x, y). If (x, y) is out of bounds or if p is null, does nothing.
+   If another piece already exists at (x, y), p will replace that piece. (This method is potentially
+   useful for creating specific test circumstances.)
+   */
+   place( piece ,  x,  y){
+     if ( this._isInbound() ) {
+       if (piece !== null){
+         this.pieces[x][y] = piece;
+       }
+     }
+   }
+
+  /*
+  Executes a remove. Returns the piece that was removed.
+  If the input (x, y) is out of bounds, returns null and prints an appropriate message.
+  If there is no piece at (x, y), returns null and prints an appropriate message.
+   */
+  remove( x,  y) {
+    if ( !(this._isInbound()) ) return null;
+    let piece = this.pieces[x][y];
+    this.pieces[x][y] = null;
+    return piece;
+  }
+  /*
+   Returns true if the square at (x, y) can be selected.
+  - A square with a piece may be selected if it is the corresponding player’s turn and one of the following is true:
+      * The player has not selected a piece yet.
+    * The player has selected a piece, but did not move it.
+
+  - An empty square may be selected if one of the following is true:
+    * During this turn, the player has selected a Piece which hasn’t moved yet and is
+      selecting an empty spot which is a valid move for the previously selected Piece.
+    * During this turn, the player has selected a Piece, captured, and has selected another
+        valid capture destination. When performing multi-captures, you should only select the active
+        piece once; all other selections should be valid destination points.
+  */
+  canSelect(x, y){
+
+  }
+
+  /*
+Selects the square at (x, y). This method assumes canSelect (x,y) returns true.
+  In respect to the GUI, when you select a Piece, color the background
+  of the selected
+  square white on the GUI via the pen color function. For any piece to
+  perform a capture,
+  that piece must have been selected first. If you select a square with
+  a piece, you are prepping
+  that piece for movement. If you select an empty square (assuming
+  canSelect returns true), you should
+  move your most recently selected piece to that square.
+  */
+
+  select( x, y){
+
+  }
+
+  /*
+  Assumes Piece p's movement from (x1, y1) to (x2, y2) is valid.
+  Moves the piece at (x1, y1), to (x2, y2) capturing any intermediate
+  piece if applicable.
+  */
+
+
+  move( x1,  y1,  x2,  y2){
+
+  }
+
+  /*
+  Returns whether or not the the current player can end their turn.
+  To be able to end a turn, a piece must have moved or performed a capture.
+  */
+  canEndTurn(){
+
+  }
+  /*
+  Called at the end of each turn. Handles switching of players and anything else that should happen at the end of a turn.
+  */
+
+  endTurn() {
+
+  }
+
+  /*
+   - Returns the winner of the game: "Fire", "Water", "Tie", or null.
+   If only fire pieces remain on the board, fire wins. If only water
+    pieces remain, water wins. If no pieces remain (consider an explosion
+    capture), no one wins and it is a tie. If the game is still in
+    progress (ie there are pieces from both sides still on the board)
+     return null. Assume there is no stalemate situation in which the
+     current player has pieces but cannot legally move any of them
+     (In this event, just leave it at null). Determine the winner solely
+     by the number of pieces belonging to each team.
+  */
+ winner(){
+
+  }
+
+}
+
+module.exports = Board;
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const Piece = __webpack_require__(1);
+
+
+class BombPiece extends Piece {
+  constructor(side,  board, pos) {
+	  super(side, board, pos);
+	  this.setPiecetype('bomb');
+  }
+
+  setKingImage(){
+    let kingImage = new Image();
+    if(this.side === 'red'){
+      kingImage.src = './asset/images/bomb-fire-crowned.png';
+    }else{
+      kingImage.src =  './asset/images/bomb-water-crowned.png';
+    }
+    return kingImage;
+  }
+
+  setPieceImage(){
+    let BombImg =  new Image();
+    if(this.side === 'red'){
+      BombImg.src = './asset/images/bomb-fire.png';
+    }else{
+      BombImg.src =  './asset/images/bomb-water.png';
+    }
+    return BombImg;
+  }
+
+  explode( x, y) {
+			  let xmin= x-1;
+			  let ymin= y-1;
+			  let xmax= x+1;
+			  let ymax= y+1;
+			  if (x === 0){
+				  xmin=x;
+			  }
+			  if (y === 0){
+				  ymin = y;
+			  }
+			  if (x === 7){
+				  xmax = x;
+						  }
+			  if (y === 7){
+				  ymax = y;
+			  }
+			  for (let i = xmin; i <= xmax; i++){
+				  for (let j = ymin; j <= ymax; j++){
+					  let b = this.getboard();
+					  if (b.getMyPieces()[i][j] !== null){
+					      b.getMyPieces()[i][j].blowUp(i,j);
+					  }
+				  }
+			  }
+		}
+
+  startCapturing() {
+     // since they explode and can't capture again...
+		this.setHaskilledthisturn(false);
+		}
+	}
+  module.exports = BombPiece;
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const Piece = __webpack_require__(1);
+
+class ShieldPiece extends Piece {
+  constructor( side, board, pos){
+	  super(side, board, pos);
+	  this.setPiecetype('shield');
+  }
+
+  setKingImage(){
+    let kingImage = new Image();
+    if(this.side === 'red'){
+      kingImage.src = './asset/images/shield-fire-crowned.png';
+    }else{
+      kingImage.src =  './asset/images/shield-water-crowned.png';
+    }
+    return kingImage;
+  }
+
+  setPieceImage(){
+    let WaterImg =  new Image();
+    if(this.side === 'red'){
+      WaterImg.src = './asset/images/shield-fire.png';
+    }else{
+      WaterImg.src =  './asset/images/shield-water.png';
+    }
+    return WaterImg;
+  }
+
+  blowUp( x, y) {
+    // does not blow up!!!
+	}
+
+}
+
+module.exports = ShieldPiece;
 
 
 /***/ })
